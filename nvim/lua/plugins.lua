01e57d4e31f91413aa1f682f617e9b5759f36e23
jinser <cmdr.jv@gmail.com>
2022-02-27 14:03:35 +0800

    zz

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -81,9 +81,43 @@ return require("packer").startup({
         -- }}}
 
         -- move {{{
-        -- use {
-        --     "edluffy/specs.nvim",
-        -- }
+        use {
+            "edluffy/specs.nvim",
+            config = function()
+                require('specs').setup{ 
+                    show_jumps  = true,
+                    min_jump = 30,
+                    popup = {
+                        delay_ms = 0, -- delay before popup displays
+                        inc_ms = 10, -- time increments used for fade/resize effects 
+                        blend = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
+                        width = 10,
+                        winhl = "PMenu",
+                        fader = require('specs').linear_fader,
+                        resizer = require('specs').shrink_resizer
+                    },
+                    ignore_filetypes = {},
+                    ignore_buftypes = {
+                        nofile = true,
+                    },
+                }
+                -- Press <C-b> to call specs!
+                vim.api.nvim_set_keymap('n', '<C-b>', ':lua require("specs").show_specs()<CR>', { noremap = true, silent = true })
+
+                -- You can even bind it to search jumping and more, example:
+                vim.api.nvim_set_keymap('n', 'n', 'n:lua require("specs").show_specs()<CR>', { noremap = true, silent = true })
+                vim.api.nvim_set_keymap('n', 'N', 'N:lua require("specs").show_specs()<CR>', { noremap = true, silent = true })
+            end
+        }
+        -- }}}
+
+        -- which-key {{{
+        use {
+          "folke/which-key.nvim",
+          config = function()
+            require("which-key").setup()
+          end
+        }
         -- }}}
     end
 })
