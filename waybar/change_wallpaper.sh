fef8c22996aad856e1064dc6f1a73165800ade54
jinser <cmdr.jv@gmail.com>
2022-07-24 16:05:59 +0800

    :bears: hyprland

--- waybar/change_wallpaper.sh
+++ waybar/change_wallpaper.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+
 PID=`pidof swaybg`
-swaybg -i $(find /home/cirno99/backup/wallpapers/pixiv/2020/. -type f | shuf -n1) -m fill &
+swaybg -i $(find /home/jinser/wallpapers/. -type f | shuf -n1) -m fill &
 sleep 1
 kill $PID
