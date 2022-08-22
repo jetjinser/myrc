c0fe561c7d555fccf8853f92b71b13112160691f
jinser <cmdr.jv@gmail.com>
2022-08-22 23:00:16 +0800

    :sparkles: add ls clangd, html, and sumneko_lua

--- nvim/lua/lsp/lsp_setup.lua
+++ nvim/lua/lsp/lsp_setup.lua
@@ -1,11 +1,16 @@
 local on_attach = require("lsp.attach").on_attach
 local lsp = require("lspconfig")
 
+
 lsp.hls.setup {
     on_attach = on_attach,
     settings = {
         formattingProvider = "ormolu"
     },
+}
+
+lsp.clangd.setup {
+    on_attach = on_attach,
 }
 
 lsp.rust_analyzer.setup {
@@ -18,4 +23,35 @@ lsp.rnix.setup {
 
 lsp.racket_langserver.setup {
     on_attach = on_attach,
+    settings = {
+        filetypes = {
+            "racket",
+        },
+    },
+}
+
+lsp.html.setup {
+    on_attach = on_attach,
+}
+
+lsp.sumneko_lua.setup {
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                globals = {
+                    "vim",
+                },
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 }
