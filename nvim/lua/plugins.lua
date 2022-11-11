3ff582b6958d68f43bc06e56decccedf1c516508
jinser <cmdr.jv@gmail.com>
2022-11-11 12:56:31 +0800

    todo-comment.nvim
    Signed-off-by: jinser <cmdr.jv@gmail.com>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -166,13 +166,21 @@ return require("packer").startup(function(use)
     -- }}}
 
     -- highlight todo {{{
-    -- use {
-    -- "folke/todo-comments.nvim",
-    -- requires = "nvim-lua/plenary.nvim",
-    -- config = function()
-    -- require("todo-comments").setup()
-    -- end
-    -- }
+    use {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        config = function()
+            require("todo-comments").setup()
+
+            vim.keymap.set("n", "]t", function()
+                require("todo-comments").jump_next()
+            end, { desc = "Next todo comment" })
+
+            vim.keymap.set("n", "[t", function()
+                require("todo-comments").jump_prev()
+            end, { desc = "Previous todo comment" })
+        end
+    }
     -- }}}
 
     -- trouble {{{
