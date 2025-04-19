fe51826fc4eff389c398c672196698a41cdee2ef
Jinser Kafka <aimer@purejs.icu>
2025-04-19 14:13:33 +0800

    ad-hoc: add script that download docsets

---
+++ download_docsets.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/.local/share/fennel-ls/docsets
+curl -o $HOME/.local/share/fennel-ls/docsets/nvim.lua https://git.sr.ht/~micampe/fennel-ls-nvim-docs/blob/main/nvim.lua
