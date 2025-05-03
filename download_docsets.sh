a0433df4655739f0d5f09e626e39135cbc6fd089
Jinser Kafka <aimer@purejs.icu>
2025-05-03 19:00:17 +0800

    refactor(downloader): use shallow clone

--- download_docsets.sh
+++ download_docsets.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 TMP=$(mktemp -d /tmp/docset-XXXXXX)
-git clone https://git.sr.ht/~micampe/fennel-ls-nvim-docs "$TMP"
+
+# shallow clone
+git clone --depth=1 https://git.sr.ht/~micampe/fennel-ls-nvim-docs "$TMP"
 
 mkdir -p $HOME/.local/share/fennel-ls/docsets
 cp "$TMP/nvim.lua" $HOME/.local/share/fennel-ls/docsets/nvim.lua
